f2fbe294e9f3a9b81d89260b6be6482109711ee2
Dakota Crane <djc2702@gmail.com>
2020-01-13 17:16:20 +0000

    First commit.

---
+++ typeclasses/characters.py
@@ -0,0 +1,52 @@
+"""
+Characters
+
+Characters are (by default) Objects setup to be puppeted by Accounts.
+They are what you "see" in game. The Character class in this module
+is setup to be the "default" character type created by the default
+creation commands.
+
+"""
+from evennia import DefaultCharacter
+from evennia.contrib.clothing import ClothedCharacter
+
+
+class Character(ClothedCharacter):
+    """
+    The Character defaults to reimplementing some of base Object's hook methods with the
+    following functionality:
+
+    at_basetype_setup - always assigns the DefaultCmdSet to this object type
+                    (important!)sets locks so character cannot be picked up
+                    and its commands only be called by itself, not anyone else.
+                    (to change things, use at_object_creation() instead).
+    at_after_move(source_location) - Launches the "look" command after every move.
+    at_post_unpuppet(account) -  when Account disconnects from the Character, we
+                    store the current location in the pre_logout_location Attribute and
+                    move it to a None-location so the "unpuppeted" character
+                    object does not need to stay on grid. Echoes "Account has disconnected"
+                    to the room.
+    at_pre_puppet - Just before Account re-connects, retrieves the character's
+                    pre_logout_location Attribute and move it back on the grid.
+    at_post_puppet - Echoes "AccountName has entered the game" to the room.
+
+    """
+
+    def at_post_puppet(self, **kwargs):
+        self.msg("\nYou become |c%s|n.\n" % self.name)
+        self.msg(self.at_look(self.location))
+
+        def message(obj, from_obj):
+            obj.msg("%s blinks their eyes." % self.get_display_name(obj), from_obj=from_obj)
+        self.location.for_contents(message, exclude=[self], from_obj=self)
+
+    def at_post_unpuppet(self, account, session=None, **kwargs):
+
+        if not self.sessions.count():
+            if self.location:
+                def message(obj, from_obj):
+                    obj.msg("%s falls to the ground, unconscious." % self.get_display_name(obj), from_obj=from_obj)
+                self.location.for_contents(message, exclude=[self], from_obj=self)
+                self.db.prelogout_location = self.location
+
+    pass
